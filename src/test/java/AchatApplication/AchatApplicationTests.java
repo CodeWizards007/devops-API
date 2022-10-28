6c08f62ccd9cee69de580f010692a159b793e462
riadhyahyaoui94 <riadh.yahyaouui.com>
2022-10-28 18:29:50 +0100

    add Testing Package

---
+++ src/test/java/AchatApplication/AchatApplicationTests.java
@@ -0,0 +1,11 @@
+package AchatApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AchatApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
